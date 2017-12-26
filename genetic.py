f6ae1c098866c80cb1e90c4190c0ae0c11369cdc
Viral Mehta <virmht@gmail.com>
2017-12-27 04:18:53 +0530

    Removed Bugs

--- genetic.py
+++ genetic.py
@@ -23,7 +23,7 @@ def get_dec(s):
     for i in range(n):
         sm+=int(s[i])
         sm=sm*2
-    return sm
+    return sm//2
 
 def get_dec_list(l):
     ret=[get_dec(i) for i in l]
@@ -33,7 +33,7 @@ def apply_selection_operator(population,fitness,binary):
     l=[i for i in fitness]
     l.sort(reverse=True)
     n=len(fitness)
-    idx=n//10
+    idx=3*n//4
     val=l[idx]
     p=[]
     f=[]
@@ -47,8 +47,11 @@ def apply_selection_operator(population,fitness,binary):
 
 def perform_crossover(s1,s2):
     n=len(s1)
+    assert(n==5)
+    assert(len(s2)==5)
     crossover_point=random.randint(1,n-1)
     ret=s1[0:crossover_point]+s2[crossover_point:]
+    assert(len(ret)==5)
     return ret
 
 def apply_crossover_operator(population,fitness,binary,num_crossovers):
@@ -56,11 +59,13 @@ def apply_crossover_operator(population,fitness,binary,num_crossovers):
     n=len(population)
     for i in range(num_crossovers):
         #print("n-1 =",n-1)
-        z=[random.randint(0,n-1),random.randint(0,n-1)]
+        z=[population[random.randint(0,n-1)],population[random.randint(0,n-1)]]
         z=get_bin(z)
         l.append(z)
     offsprings_bin=[perform_crossover(i[0],i[1]) for i in l]
     offsprings_dec=get_dec_list(offsprings_bin)
+    for i in offsprings_dec:
+        assert(i<32)
     offsprings_fitness=get_fitness(offsprings_dec)
     population+=offsprings_dec
     fitness+=offsprings_fitness
@@ -68,7 +73,7 @@ def apply_crossover_operator(population,fitness,binary,num_crossovers):
     return (population,fitness,binary)
 
 def apply_mutation_operator(population,fitness,binary):
-    if(random.random()>0.1):
+    if(random.random()>0.4):
         idx=random.randint(0,len(population)-1)
         bit=random.randint(0,len(binary[0])-1)
         binary[idx]=binary[idx][0:bit]+str(random.randint(0,1))+binary[idx][bit+1:]
@@ -80,31 +85,33 @@ def avg(l):
     return sum(l)/len(l)
 
 def hash1(l):
-    h=[0]*21
+    h=[0]*33
     for i in l:
         h[i]+=1
     return h
 
 # Find argmin (x-10)**2, 0<=x<=20
 
-num_gens=100
+num_gens=20
 ans=[]
-mx=-100000000000
-population = get_initial_population(size=10000)
+mx=-10**100
+population = get_initial_population(size=100000)
 for i in range(num_gens):
     n=len(population)
     #random.shuffle(population)
     fitness=get_fitness(population)
     avg_fitness=avg(fitness)
     binary=get_bin(population)
-    population,fitness,binary=apply_selection_operator(population,fitness,binary)
     if(mx<avg_fitness):
         mx=avg_fitness
         ans=[j for j in population]
+    population,fitness,binary=apply_selection_operator(population,fitness,binary)
     num_crossovers=n-len(population)
     population,fitness,binary=apply_crossover_operator(population,fitness,binary,num_crossovers)
     #print("generation = "+str(i+1)+" avg_fitness = "+str(avg_fitness)+" n = "+str(n)+" population =",population)
-    #assert(len(binary[0])==5)
+    print("generation = "+str(i+1)+" avg_fitness = "+str(avg_fitness)+" n = "+str(n))
+    assert(len(binary[0])==5 and max(population)<=32)
     population,fitness,binary=apply_mutation_operator(population,fitness,binary)
 print("###########################")
-print(mx)
+print("Max fitness =",mx)
+#print("Fittest Population =",ans)
